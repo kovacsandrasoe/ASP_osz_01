2e813c502e2fde350af37e01170ffc022684da41
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2019-09-11 19:10:17 +0200

    all

---
+++ ASPFirstApp/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPFirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Szamolo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Szamolo(int min, int step, int max)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = min; i <= max; i+= step)
+            {
+                numbers.Add(i);
+            }
+            return View("Result", numbers);
+        }
+
+        public IActionResult Items()
+        {
+            return View();
+        }
+
+        public IActionResult Greetings()
+        {
+            return View();
+        }
+
+        public IActionResult Sorsolas()
+        {
+            string tanar = Models.Sorsolo.Sorsol();
+            //melyik view-nak és mit?
+            //return View("Sorsolas", tanar);
+            return View(nameof(Sorsolas), tanar);
+        }
+    }
+}
---
+++ ASPFirstApp/Models/Sorsolo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPFirstApp.Models
+{
+    public static class Sorsolo
+    {
+        static string[] nevek = {"Csink", "Vámossy", "Sergyán", "Szabó Zsé", "Kiss Mari", "Tick",
+        "Vígh"};
+
+        static Random r = new Random();
+
+        public static string Sorsol()
+        {
+            return nevek[r.Next(0, nevek.Length)];
+        }
+    }
+}
